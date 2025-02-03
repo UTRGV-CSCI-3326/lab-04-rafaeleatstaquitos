0b971065d19883f5ed7eea567d89d720a21cb9ca
Rafael Oviedo <169010054+rafaeleatstaquitos@users.noreply.github.com>
2025-02-03 02:20:45 +0000

    Done

--- src/Converter.java
+++ src/Converter.java
@@ -1,1 +1,14 @@
-
+import java.util.Scanner;
+public class Converter {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Feet-to-Yards Lenght Converter!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a lenght in feet: ");
+        float feet = input.nextFloat();
+        input.close();
+        System.out.println();
+        float yards = feet / 3;
+        System.out.printf("%.1f feet is %.2f yards%n", feet, yards);
+        System.out.println();
+    }
+}
